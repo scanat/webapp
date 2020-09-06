c23ba15d53f8efe82179b582972faf218779b2a4
Omkar Deshmukh <iamomkar666@gmail.com>
2020-09-06 21:36:24 +0530

    beta-v3

--- src/components/main/banner.js
+++ src/components/main/banner.js
@@ -1,57 +1,56 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import BannerStyles from "./banner.module.css"
 import BannerImage from "../../images/bannerimage.jpg"
-import Anime from 'animejs'
+import Anime from "animejs"
 
 const Banner = () => {
-
-    useEffect(() => {
-        Anime({
-            targets: document.getElementById('animatingBanner'),
-            keyframes: [
-                {
-                    translateX: 0,
-                    duration: 2000
-                },
-                {
-                    translateX: [0, '-50%'],
-                    duration: 2000
-                },
-                {
-                    translateX: '-50%',
-                    duration: 2000
-                },
-                {
-                    translateX: ['-50%', 0],
-                    duration: 2000
-                },
-                {
-                    translateX: 0,
-                    duration: 2000
-                }
-            ],
-            autoplay: true,
-            easing: 'easeInOutCubic',
-            loop: true,
-            direction: 'alternate'
-        })
+  const [smallScreen, setSmallScreen] = useState(true)
+  useEffect(() => {
+    window.innerWidth < 992 ? setSmallScreen(true) : setSmallScreen(false)
+    Anime({
+      targets: document.getElementById("animatingBanner"),
+      keyframes: [
+        {
+          translateX: 0,
+          duration: 2000,
+        },
+        {
+          translateX: [0, "-50%"],
+          duration: 2000,
+        },
+        {
+          translateX: "-50%",
+          duration: 2000,
+        },
+        {
+          translateX: ["-50%", 0],
+          duration: 2000,
+        },
+        {
+          translateX: 0,
+          duration: 2000,
+        },
+      ],
+      autoplay: true,
+      easing: "easeInOutCubic",
+      loop: true,
+      direction: "alternate",
     })
+  })
 
-  if (typeof window !== "undefined") {
-    if (window.innerWidth >= 992) {
-      return (
-        <section className={BannerStyles.container}>
-          <img src={BannerImage} style={{ width: "100%", float: "left" }} />
-        </section>
-      )
-    } else {
-      return (
-        <section className={BannerStyles.container}>
-          <img id='animatingBanner' src={BannerImage} style={{ width: "200%", float: "left" }} />
-        </section>
-      )
-    }
-  }
+  return (
+    <section className={BannerStyles.container}>
+      {smallScreen ? (
+        <img
+          id="animatingBanner"
+          src={BannerImage}
+          style={{ width: "200%", float: "left" }}
+        />
+      ) : (
+        <img src={BannerImage} style={{ width: "100%", float: "left" }} />
+      )}
+    </section>
+  )
 }
 
 export default Banner
