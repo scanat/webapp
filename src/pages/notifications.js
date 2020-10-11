3d77b70747d0465bf2c2567d817a950b554ccb44
Omkar Deshmukh <iamomkar666@gmail.com>
2020-10-11 14:48:24 +0530

    prod-v6

--- src/pages/notifications.js
+++ src/pages/notifications.js
@@ -5,11 +5,11 @@ import { graphql } from "gatsby"
 import { API, graphqlOperation } from "aws-amplify"
 import * as subscriptions from "../graphql/subscriptions"
 import * as queries from "../graphql/queries"
-import {awsNotificationConfig} from "../aws-exports"
+import { awsNotificationConfig } from "../aws-exports"
 
 API.configure(awsNotificationConfig)
 
-const Notifications = ({ data }) => {
+const Notifications = () => {
   const [notificationsList, setNotificationsList] = useState([])
 
   useEffect(() => {
@@ -17,7 +17,7 @@ const Notifications = ({ data }) => {
   }, [])
 
   useEffect(() => {
-    const subscription = API.graphql(
+    const onCreateSubscription = API.graphql(
       graphqlOperation(subscriptions.onCreateTodo)
     ).subscribe({
       next: data => {
@@ -26,11 +26,23 @@ const Notifications = ({ data }) => {
       },
       error: err => console.log(err),
     })
+
+    const onUpdateSubscription = API.graphql(
+      graphqlOperation(subscriptions.onUpdateTodo)
+    ).subscribe({
+      next: data => {
+        console.log(data.value.data)
+        refetchQl()
+      },
+      error: err => console.log(err)
+    })
   }, [])
 
   async function refetchQl() {
     try {
-      const notifications = await API.graphql({ query: queries.listNotifications })
+      const notifications = await API.graphql({
+        query: queries.listNotifications,
+      })
       setNotificationsList(notifications.data.listTodos.items)
     } catch (error) {
       console.log(error)
@@ -54,17 +66,3 @@ const Notifications = ({ data }) => {
 }
 
 export default Notifications
-
-export const dataQuery = graphql`
-  query notificationsQuery {
-    notifications {
-      listTodos(filter: { status: { eq: true } }) {
-        items {
-          description
-          topic
-          status
-        }
-      }
-    }
-  }
-`
